45dddd14862aaf9be5353295c9760f0690ecab37
PJ Hoberman <pj.hoberman@gmail.com>
2014-06-06 09:50:45 -0600

    working on og

--- wp-content/scripts/functions.php
+++ wp-content/scripts/functions.php
@@ -39,15 +39,23 @@ function prefix_url_rewrite_templates() {
             return get_stylesheet_directory() . '/page-templates/event-details.php';
         });
 
+        // todo - might make sense to move this to a different function...
         require_once($_SERVER['DOCUMENT_ROOT'] . "/wp-content/scripts/nightout.php");
         add_action('wp_head', function(){
             global $event_slug, $this_event;
             $this_event = getThisEvent($event_slug);
 
+            $event_description = $this_event -> description;
+            if( $event_description === "" ) {
+                $event_description = ": Imbibe is craft events. We specialize in craft beer and spirit events and ticketing.";
+            }
+            $event_description = $this_event -> title . " - " . $event_description;
+            $event_description = htmlspecialchars($event_description);
+
             ?>
          <!-- for Google -->
-        <meta name="description" content="Imbibe is craft events. We specialize in craft beer and spirit events and ticketing." />
-        <meta name="keywords" content="craft, events, craft beer, spirits, ticketing, tickets, festivals" />
+        <meta name="description" content="<?= $event_description ?>" />
+        <meta name="keywords" content="<?= $this_event -> title ?>, craft, events, craft beer, spirits, ticketing, tickets, festivals" />
 
         <meta name="author" content="Imbibe" />
         <meta name="copyright" content="<?= date("Y"); ?>" />
@@ -56,22 +64,21 @@ function prefix_url_rewrite_templates() {
         <!-- for Facebook -->
 
         <meta property="og:title" content="<?= $this_event -> title; ?>" />
-        <meta property="og:type" content="event" />
         <meta property="og:image" content="<?= $this_event -> poster_url -> large ?>" />
         <meta property="og:url" content="<?= get_site_url() . $_SERVER['REQUEST_URI']; ?>" />
-        <meta property="og:description" content="" />
+        <meta property="og:description" content="<?= $event_description ?>" />
 
         <!-- for Twitter -->
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content="<?= get_the_title(); ?>" />
-        <meta name="twitter:description" content="" />
+        <meta name="twitter:title" content="<?= $this_event -> title ?>" />
+        <meta name="twitter:description" content="<?= $event_description ?>" />
         <meta name="twitter:image" content="<?= $this_event -> poster_url -> large ?>" />
 
         <title><?php wp_title( '&#124;', true, 'right' ); ?></title>
             <?
         });
 
-    //Remover YOAST na página de empresas
+    //Remover YOAST settings on these pages
     add_filter( 'wpseo_canonical', 'wpseo_canonical_exclude' );
     function wpseo_canonical_exclude( $canonical ) {
         $canonical = false;
@@ -82,7 +89,12 @@ function prefix_url_rewrite_templates() {
         $description = false;
         return $description;
     }
-
+    add_filter( 'wpseo_title', 'wpseo_title_exclude' );
+    function wpseo_title_exclude( $title ) {
+        $title = false;
+        return $title;
+    }
+    // end of making sense
 
     }
 }
